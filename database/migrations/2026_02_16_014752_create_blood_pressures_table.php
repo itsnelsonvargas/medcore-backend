3b4292cd88a46eeafc9ad49c576742acf370b703
nelson <itsnelsonvargas@gmail.com>
2026-02-16 09:51:10 +0800

    feat: add a table for blood pressures for recording purposes

---
+++ database/migrations/2026_02_16_014752_create_blood_pressures_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('blood_pressures', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger('patient_id');
+            $table->integer('systolic');
+            $table->integer('diastolic');
+            $table->foreign('patient_id')->references('id')->on('patients')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('blood_pressures');
+    }
+};

5bacccf6f8c0be4cc6b652ee58d08b6372189c49
nelson <itsnelsonvargas@gmail.com>
2026-03-01 13:26:38 +0800

    feat: create a group of endpoints for clients

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,32 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Group routes under /v1
+Route::prefix('v1')->group(function () {
+    
+    // Public routes
+    Route::post('/login', [AuthController::class, 'login']);
+
+    // Protected routes
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('/user', function (Request $request) {
+            return $request->user(); 
+        });
+        
+
+        // Client Routes
+        Route::prefix('clients')->group(function () {
+            Route::get('/', [ClientController::class, 'index']);            
+            Route::get('/{id}', [ClientController::class, 'show']);
+            Route::put('/{id}', [ClientController::class, 'update']);
+            Route::delete('/{id}', [ClientController::class, 'destroy']);
+        });
+
+
+
+        Route::post('/logout', [AuthController::class, 'logout']);
+    });
+});
